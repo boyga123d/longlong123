ed1d3202921dd68de734ace79d799312bdd3efe9
boyga123d <72071157+boyga123d@users.noreply.github.com>
2020-10-17 14:14:42 +0700

    baitap

---
+++ wpf-mvvm/wpf-mvvm/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_mvvm
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private string _name;
+        private string _helloText;
+
+        
+
+        public string Name {
+            get
+            {
+                return _name;
+            }
+            set{
+                _name = value;
+                HelloText = $"Hello {_name}";
+            }
+        }
+
+        public string HelloText { get { return _helloText; }
+            set {
+                _helloText = value;
+                OnPropertyChanged(nameof(HelloText));
+            }
+        }
+    }
+}
